f11808eb18187fa666d0a353c4d3aa9adf471295
dzsigus <daniel.zsigus@everit.biz>
2023-01-04 23:49:05 +0100

    design and performance impovements

--- src/components/SprinterCardSelector.vue
+++ src/components/SprinterCardSelector.vue
@@ -12,9 +12,16 @@ const { selectSprinter } = store
 
 <template>
     <div class="my-5 text-2xl">{{ $t('select_card_for_sprinter') }}</div>
-    <div class="flex flex-wrap justify-center">
-        <CardItem v-for="(card, i) in drawnSprinters" :key="i" @click="selectSprinter(card)">
-            <div class="text-8xl">{{ card.value }}</div>
+    <div class="flex w-80 flex-wrap items-center justify-around align-middle">
+        <CardItem
+            v-for="(card, i) in drawnSprinters"
+            :key="i"
+            class="bg-violet-700 hover:bg-violet-800"
+            @click="selectSprinter(card)"
+        >
+            <div class="flex h-full -translate-y-2 flex-col justify-center text-8xl">
+                {{ card.value }}
+            </div>
         </CardItem>
     </div>
 </template>
